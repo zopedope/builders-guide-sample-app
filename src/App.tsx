d23123f92318900c07afed8164fdd0343dde0da9
jamaljsr <1356600+jamaljsr@users.noreply.github.com>
2020-10-05 21:50:57 -0400

    initial app before any LN features

---
+++ src/App.tsx
@@ -0,0 +1,36 @@
+import React, { ReactNode } from 'react';
+import { Alert, Container, Navbar } from 'react-bootstrap';
+import { observer } from 'mobx-react-lite';
+import CreatePost from './pages/CreatePost';
+import PostList from './pages/PostList';
+import { useStore } from './store/Provider';
+
+function App() {
+  const store = useStore();
+
+  const pages: Record<string, ReactNode> = {
+    posts: <PostList />,
+    create: <CreatePost />,
+  };
+
+  return (
+    <>
+      <Navbar bg="dark" variant="dark" expand="md">
+        <Navbar.Brand onClick={store.gotoPosts}>
+          Builder's Guide to the LND Galaxy
+        </Navbar.Brand>
+      </Navbar>
+
+      <Container className="my-3">
+        {store.error && (
+          <Alert variant="danger" dismissible onClose={store.clearError}>
+            {store.error}
+          </Alert>
+        )}
+        {pages[store.page]}
+      </Container>
+    </>
+  );
+}
+
+export default observer(App);
